83e563d4898305c7fb549f812ade7f343be1d002
dipen_solulab <dipen@solulab.com>
2020-01-09 13:18:38 +0530

    List View Implementation : Completed

--- lib/ListViewJsonapi.dart
+++ lib/ListViewJsonapi.dart
@@ -4,13 +4,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
 class ListViewJsonApi extends StatefulWidget {
   _ListViewJsonApiState createState() => _ListViewJsonApiState();
 }
 
 class _ListViewJsonApiState extends State<ListViewJsonApi> {
-  final String uri = 'http://api.github.com/repos/deepandroid/github-integration-flutter/commits';
+  final String uri =
+      'http://api.github.com/repos/deepandroid/github-integration-flutter/commits';
 
   Future<List<Commits>> _fetchUsers() async {
     var response = await http.get(uri);
@@ -18,14 +18,10 @@ class _ListViewJsonApiState extends State<ListViewJsonApi> {
     if (response.statusCode == 200) {
       final items = json.decode(response.body).cast<Map<String, dynamic>>();
 
-      print("+++++++++++++++++++++++++++ items ++++++++++++++++++++++++++");
-      print(items);
       List<Commits> listOfUsers = items.map<Commits>((json) {
         return Commits.fromJson(json);
       }).toList();
 
-      print("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
-      print(listOfUsers);
       return listOfUsers;
     } else {
       throw Exception('Failed to load internet');
@@ -41,25 +37,46 @@ class _ListViewJsonApiState extends State<ListViewJsonApi> {
       body: FutureBuilder<List<Commits>>(
         future: _fetchUsers(),
         builder: (context, snapshot) {
-          if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
-
-          return ListView(
-            children: snapshot.data
-                .map((user) => ListTile(
-              title: Text(user.commit.author.name),
-              subtitle: Text(user.commit.message),
-              leading: CircleAvatar(
-                backgroundColor: Colors.red,
-                child: Text(user.commit.author.name[0],
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      color: Colors.white,
-                    )),
-              ),
-            ))
-                .toList(),
+          if (!snapshot.hasData)
+            return Center(child: CircularProgressIndicator());
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index) {
+              Commits commits = snapshot.data[index];
+              return Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Column(
+                          // align the text to the left instead of centered
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              commits.commit.author.name,
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Text(commits.commit.message),
+                            Text(commits.commit.author.date.replaceAll("T"," ").replaceAll("Z", "")),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
           );
         },
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await _fetchUsers();
+        },
+        tooltip: 'Github API calling',
+        child: Icon(Icons.cached),
       ),
     );
   }
@@ -79,7 +96,7 @@ class Commits {
   factory Commits.fromJson(Map<String, dynamic> json) {
     return Commits(
       sha: json['sha'],
-      commit: json['commit'],
+      commit: Commit.fromJson(json["commit"]),
       nodeId: json['node_id'],
     );
   }
@@ -89,14 +106,11 @@ class Commit {
   Author author;
   String message;
 
-  Commit({
-    this.author,
-    this.message
-  });
+  Commit({this.author, this.message});
 
   factory Commit.fromJson(Map<String, dynamic> json) {
     return Commit(
-      author: json['author'],
+      author: Author.fromJson(json['author']),
       message: json['message'],
     );
   }
@@ -106,10 +120,7 @@ class Author {
   String name;
   String date;
 
-  Author({
-    this.name,
-    this.date
-  });
+  Author({this.name, this.date});
 
   factory Author.fromJson(Map<String, dynamic> json) {
     return Author(
